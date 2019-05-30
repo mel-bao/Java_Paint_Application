3f4a104c40cbdfe3dca8e1d7edd8ff223b0b5ee6
Melanie Howard <n9736247@qut.edu.au>
2019-05-30 14:30:38 +1000

    save file in vec format not including pen or fill

--- src/a2/FileManager.java
+++ src/a2/FileManager.java
@@ -5,7 +5,45 @@ import java.util.ArrayList;
 public class FileManager {
 
     public String SaveFile(ArrayList<ArrayList> listOfShapes) {
-        return "testing method";
+        String string = "";
+
+        for (ArrayList draw: listOfShapes) {
+            String shape;
+            String pen = null;
+            String fill = null;
+            String coord = "";
+
+            if (draw.get(0) instanceof PlotDraw) {
+                shape = "PLOT ";
+                coord = draw.get(1) + " " + draw.get(2);
+                string += shape + coord + "\n";
+            }else if (draw.get(0) instanceof LineDraw) {
+                shape = "LINE ";
+                coord = draw.get(1) + " " + draw.get(2) + " " + draw.get(3)+ " " + draw.get(4);
+                string += shape + coord + "\n";
+            } else if (draw.get(0) instanceof RectangleDraw) {
+                shape = "RECTANGLE ";
+                coord = draw.get(1) + " " + draw.get(2) + " " + draw.get(3)+ " " + draw.get(4);
+                string += shape + coord + "\n";
+            } else if (draw.get(0) instanceof EllipseDraw) {
+                shape = "ELLIPSE ";
+                coord = draw.get(1) + " " + draw.get(2) + " " + draw.get(3)+ " " + draw.get(4);
+                string += shape + coord + "\n";
+            } else if (draw.get(0) instanceof PolygonDraw) {
+                shape = "POLYGON ";
+                for (int i=1; i < (draw.size() - 2); i++) {
+                    if (i == (draw.size() - 3)) {
+                        coord += draw.get(i);
+                    } else {
+                        coord += draw.get(i) + " ";
+                    }
+                }
+                string += shape + coord + "\n";
+            }
+
+        }
+
+        return string;
     }
 
 }
